98e5fcafbe98c208b16e80ed65141a36a5a7e585
Mikołaj Legutko <7241679@wp.pl>
2022-02-20 20:21:01 +0100

    Added grass

--- apps/engine/src/app/modules/MapModule/mapSchema.ts
+++ apps/engine/src/app/modules/MapModule/mapSchema.ts
@@ -1,6 +1,7 @@
 const BLOCK_SIZE = 32;
 
 export const mapSchema = {
+   // Grass
    '1': {
       path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
       location: {
@@ -8,4 +9,97 @@ export const mapSchema = {
          x: 4 * BLOCK_SIZE,
       },
    },
+   '2': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 5 * BLOCK_SIZE,
+      },
+   },
+   '3': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 6 * BLOCK_SIZE,
+      },
+   },
+   '4': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 7 * BLOCK_SIZE,
+      },
+   },
+   '5': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 8 * BLOCK_SIZE,
+      },
+   },
+   '6': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 9 * BLOCK_SIZE,
+      },
+   },
+   '7': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 10 * BLOCK_SIZE,
+      },
+   },
+   '8': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 4 * BLOCK_SIZE,
+         x: 11 * BLOCK_SIZE,
+      },
+   },
+   '9': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 5 * BLOCK_SIZE,
+         x: 0 * BLOCK_SIZE,
+      },
+   },
+   '10': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 5 * BLOCK_SIZE,
+         x: 1 * BLOCK_SIZE,
+      },
+   },
+   '11': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 5 * BLOCK_SIZE,
+         x: 3 * BLOCK_SIZE,
+      },
+   },
+   '12': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 5 * BLOCK_SIZE,
+         x: 4 * BLOCK_SIZE,
+      },
+   },
+   '13': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 5 * BLOCK_SIZE,
+         x: 7 * BLOCK_SIZE,
+      },
+   },
+   '14': {
+      path: '../assets/spritesheets/mapTextures/Sprites-110575-110718.png',
+      location: {
+         y: 5 * BLOCK_SIZE,
+         x: 8 * BLOCK_SIZE,
+      },
+   },
+
+   // Hik
 };
