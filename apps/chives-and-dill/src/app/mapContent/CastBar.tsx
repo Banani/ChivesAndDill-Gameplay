0e29a6d521b1846cc83ca5f1ddd3b0c390717b67
Mikołaj Legutko <7241679@wp.pl>
2022-07-16 18:50:19 +0200

    Fixed not visible cast bar

--- apps/chives-and-dill/src/app/mapContent/CastBar.tsx
+++ apps/chives-and-dill/src/app/mapContent/CastBar.tsx
@@ -1,44 +1,45 @@
 import React, { useEffect, useCallback, useState } from 'react';
-import { useSelector } from 'react-redux';
 import { Graphics } from '@inlet/react-pixi';
-import { getEngineState } from '../../stores';
 
-export const CastBar = ({ playerId, castBarData }) => {
+export const CastBar = React.memo<{ playerId: any; castBarData: any; location: { x: number; y: number } }>(
+   ({ playerId, castBarData, location }) => {
 
-   const [channelSpellProgress, updateChannelSpellProgress] = useState(0);
+      const [channelSpellProgress, updateChannelSpellProgress] = useState(0);
 
-   const engineState = useSelector(getEngineState);
+      useEffect(() => {
+         const interval = setInterval(() => {
+            if (castBarData) {
+               const { castingStartedTimestamp, timeToCast } = castBarData;
+               updateChannelSpellProgress((Date.now() - castingStartedTimestamp) / timeToCast);
+            }
+         }, 1000 / 60);
 
-   useEffect(() => {
-      const interval = setInterval(() => {
-         if (castBarData) {
-            const { castingStartedTimestamp, timeToCast } = castBarData;
-            updateChannelSpellProgress((Date.now() - castingStartedTimestamp) / timeToCast);
-         }
-      }, 1000 / 60);
+         updateChannelSpellProgress(0);
+         return () => {
+            clearInterval(interval);
+         };
+      }, [castBarData, playerId]);
 
-      updateChannelSpellProgress(0);
-      return () => {
-         clearInterval(interval);
-      };
-   }, [castBarData, playerId]);
-
-   const castBar = useCallback(
-      (g) => {
-         g.clear();
-         g.beginFill(0xcfcfcf);
-         g.drawRect(engineState?.characterMovements.data[playerId].location.x + 20, engineState?.characterMovements.data[playerId].location.y + 80, 50, 5);
-         g.endFill();
-         g.beginFill(0x2372fa);
-         g.drawRect(
-            engineState?.characterMovements.data[playerId].location.x + 20,
-            engineState?.characterMovements.data[playerId].location.y + 80,
-            (channelSpellProgress * 100) / 2,
-            5
-         );
-         g.endFill();
-      },
-      [playerId, channelSpellProgress, engineState.characterMovements]
-   );
-   return <Graphics draw={castBar} />;
-};
+      const castBar = useCallback(
+         (g) => {
+            g.clear();
+            g.beginFill(0xcfcfcf);
+            g.drawRect(location.x + 20, location.y + 80, 50, 5);
+            g.endFill();
+            g.beginFill(0x2372fa);
+            g.drawRect(
+               location.x + 20,
+               location.y + 80,
+               (channelSpellProgress * 100) / 2,
+               5
+            );
+            g.endFill();
+         },
+         [playerId, channelSpellProgress, location]
+      );
+      return <Graphics draw={castBar} />;
+   },
+   (old, newProps) =>
+      old.location.x === newProps.location.x &&
+      old.location.y === newProps.location.y
+);
