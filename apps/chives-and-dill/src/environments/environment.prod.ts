2815ee8b0af005fd32c3483a05d36309469536b0
psycholo96@gmail.com <psycholo96@gmail.com>
2022-07-29 23:31:15 +0200

    port changed

--- apps/chives-and-dill/src/environments/environment.prod.ts
+++ apps/chives-and-dill/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
    production: true,
-   engineUrl: 'http://ec2-54-147-45-103.compute-1.amazonaws.com/',
+   engineUrl: 'http://ec2-54-147-45-103.compute-1.amazonaws.com:3000',
 };
