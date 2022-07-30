d5969bbdbb736fed2d4b0f8b94332d071af3a095
psycholo96@gmail.com <psycholo96@gmail.com>
2022-07-30 12:54:23 +0200

    CORS changed

--- apps/engine/src/environments/environment.prod.ts
+++ apps/engine/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
    production: true,
    hostname: 'ec2-54-147-45-103.compute-1.amazonaws.com',
-   origin: 'https://master.d39cy3kjazccax.amplifyapp.com/',
+   origin: 'master.d39cy3kjazccax.amplifyapp.com/',
 };
