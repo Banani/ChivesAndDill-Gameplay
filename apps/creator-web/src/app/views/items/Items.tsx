2ca771dfd946034629899e24b39a2aab0ac43861
psycholo96@gmail.com <psycholo96@gmail.com>
2023-01-26 23:15:21 +0100

    item templates filtering

--- apps/creator-web/src/app/views/items/Items.tsx
+++ apps/creator-web/src/app/views/items/Items.tsx
@@ -1,5 +1,7 @@
+import { ItemTemplate } from '@bananos/types';
 import AddIcon from '@mui/icons-material/Add';
 import { Button, Paper } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import _ from 'lodash';
 import { useContext, useState } from 'react';
 import { PackageContext } from '../../contexts';
@@ -13,23 +15,44 @@ export const Items = () => {
    const packageContext = useContext(PackageContext);
    const [paginationRange, setPaginationRange] = useState({ start: 0, end: 0 });
    const [paginationReset, setPaginationReset] = useState(0);
+   const [searchFilter, setSearchFilter] = useState('');
 
    const itemTemplates = packageContext?.backendStore?.itemTemplates?.data ? packageContext?.backendStore?.itemTemplates?.data : {};
 
    return (
       <Paper className={styles['map-editor']}>
-         <Button variant="outlined" onClick={() => setActiveDialog(Dialogs.ItemDialog)}>
-            <AddIcon />
-         </Button>
+         <div className={styles['manage-panel']}>
+            <TextField
+               value={searchFilter}
+               onChange={(e) => {
+                  setSearchFilter(e.target.value);
+                  setPaginationReset((prev) => prev + 1);
+               }}
+               margin="dense"
+               label="Search by name"
+               fullWidth
+               variant="standard"
+               type="text"
+            />
+
+            <Button className={styles['add-item-template-button']} variant="outlined" onClick={() => setActiveDialog(Dialogs.ItemDialog)}>
+               <AddIcon />
+            </Button>
+         </div>
 
          <div className={styles['list-holder']}>
             <div className={styles['list']}>
-               {_.map(itemTemplates, (itemTemplate) => (
-                  <div key={itemTemplate.id} className={styles['list-item']}>
-                     <img className={styles['image-preview']} src={itemTemplate.image} />
-                     <div className={styles['bar']}>{itemTemplate.name}</div>
-                  </div>
-               ))}
+               {_.map(
+                  Object.values<ItemTemplate>(itemTemplates)
+                     .filter((itemTemplate: ItemTemplate) => itemTemplate.name.toLowerCase().indexOf(searchFilter.toLowerCase()) !== -1)
+                     .slice(paginationRange.start, paginationRange.end),
+                  (itemTemplate) => (
+                     <div key={itemTemplate.id} className={styles['list-item']}>
+                        <img className={styles['image-preview']} src={itemTemplate.image} />
+                        <div className={styles['bar']}>{itemTemplate.name}</div>
+                     </div>
+                  )
+               )}
             </div>
          </div>
          <div className={styles['pagination-holder']}>
