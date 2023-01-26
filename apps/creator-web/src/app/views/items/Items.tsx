60fa7b317fafebfe6ac10951e6e9cd27709da676
psycholo96@gmail.com <psycholo96@gmail.com>
2023-01-26 21:48:52 +0100

    Pagination for itemTemplates list

--- apps/creator-web/src/app/views/items/Items.tsx
+++ apps/creator-web/src/app/views/items/Items.tsx
@@ -1,15 +1,18 @@
 import AddIcon from '@mui/icons-material/Add';
 import { Button, Paper } from '@mui/material';
 import _ from 'lodash';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PackageContext } from '../../contexts';
 import { DialogContext, Dialogs } from '../../contexts/dialogContext';
+import { Pagination } from '../components';
 
 import styles from './Items.module.scss';
 
 export const Items = () => {
    const { setActiveDialog } = useContext(DialogContext);
    const packageContext = useContext(PackageContext);
+   const [paginationRange, setPaginationRange] = useState({ start: 0, end: 0 });
+   const [paginationReset, setPaginationReset] = useState(0);
 
    const itemTemplates = packageContext?.backendStore?.itemTemplates?.data ? packageContext?.backendStore?.itemTemplates?.data : {};
 
@@ -19,13 +22,18 @@ export const Items = () => {
             <AddIcon />
          </Button>
 
-         <div className={styles['list']}>
-            {_.map(itemTemplates, (itemTemplate) => (
-               <div key={itemTemplate.id} className={styles['list-item']}>
-                  <img className={styles['image-preview']} src={itemTemplate.image} />
-                  <div className={styles['bar']}>{itemTemplate.name}</div>
-               </div>
-            ))}
+         <div className={styles['list-holder']}>
+            <div className={styles['list']}>
+               {_.map(itemTemplates, (itemTemplate) => (
+                  <div key={itemTemplate.id} className={styles['list-item']}>
+                     <img className={styles['image-preview']} src={itemTemplate.image} />
+                     <div className={styles['bar']}>{itemTemplate.name}</div>
+                  </div>
+               ))}
+            </div>
+         </div>
+         <div className={styles['pagination-holder']}>
+            <Pagination itemsAmount={Object.values(itemTemplates).length} setRange={setPaginationRange} reset={paginationReset} />
          </div>
       </Paper>
    );
