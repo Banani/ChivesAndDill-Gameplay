8d2952988a01dfe4bc256531f284284c515916ac
Mikołaj Legutko <7241679@wp.pl>
2023-07-16 14:02:05 +0200

    Added disabled effect for spell icon when player doesn't have enough spell power

--- apps/chives-and-dill/src/app/guiContent/spellsBar/SpellsBar.tsx
+++ apps/chives-and-dill/src/app/guiContent/spellsBar/SpellsBar.tsx
@@ -1,33 +1,50 @@
-import { GlobalStoreModule } from '@bananos/types';
+import { GlobalStoreModule, Player, PowerPointsTrack } from '@bananos/types';
 import { KeyBoardContext } from 'apps/chives-and-dill/src/contexts/KeyBoardContext';
 import { useEngineModuleReader, useSpellDefinitionProvider } from 'apps/chives-and-dill/src/hooks';
 import classnames from 'classnames';
 import _ from 'lodash';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import styles from './SpellsBar.module.scss';
 import { SpellLoader } from './components/SpellLoader';
 
 interface SpellsBarProps {
     lastUpdateTime: string;
     availableSpells: Record<string, boolean>;
-    spellCastTime: Record<string, number>
+    spellCastTime: Record<string, number>;
+    characterPowerPoints: Record<string, PowerPointsTrack>;
+    activeCharacterId: string;
+    characters: Record<string, Player>;
 }
 
 export const SpellsBar = () => {
     const { data: availableSpells, lastUpdateTime: availableSpellsUpdateTime } = useEngineModuleReader(GlobalStoreModule.AVAILABLE_SPELLS);
     const { data: spellCastTime, lastUpdateTime: spellCastTimeUpdateTime } = useEngineModuleReader(GlobalStoreModule.SPELL_CAST_TIME);
+    const { data: characterPowerPoints, lastUpdateTime: characterPowerPointsTime } = useEngineModuleReader(GlobalStoreModule.CHARACTER_POWER_POINTS);
+    const { activeCharacterId } = useEngineModuleReader(GlobalStoreModule.ACTIVE_CHARACTER).data;
+    const { data: characters } = useEngineModuleReader(GlobalStoreModule.CHARACTER);
 
     return <InternalSpellsBar
-        lastUpdateTime={availableSpellsUpdateTime.toString() + spellCastTimeUpdateTime.toString()}
+        lastUpdateTime={availableSpellsUpdateTime.toString() + spellCastTimeUpdateTime.toString() + characterPowerPointsTime.toString()}
         availableSpells={availableSpells as Record<string, boolean>}
         spellCastTime={spellCastTime as Record<string, number>}
+        characterPowerPoints={characterPowerPoints as Record<string, PowerPointsTrack>}
+        activeCharacterId={activeCharacterId}
+        characters={characters as Record<string, Player>}
     />
 }
 
-const InternalSpellsBar: React.FunctionComponent<SpellsBarProps> = React.memo(({ availableSpells, spellCastTime }) => {
+const InternalSpellsBar: React.FunctionComponent<SpellsBarProps> = React.memo(({ availableSpells, spellCastTime, characterPowerPoints, activeCharacterId, characters }) => {
     const keyBoardContext = useContext(KeyBoardContext);
     const { spellDefinitions } = useSpellDefinitionProvider({ spellDefinitionIds: Object.keys(availableSpells) });
     const [clickedKey, setClickedKey] = useState('');
+
+    const checkIfDisabled = useCallback((powerCost) => {
+        if (powerCost > characterPowerPoints[activeCharacterId].currentSpellPower) {
+            return 'grayscale(70%)';
+        }
+
+        return '';
+    }, [characterPowerPoints[activeCharacterId].currentSpellPower]);
 
     useEffect(() => {
         keyBoardContext.addKeyHandler({
@@ -59,9 +76,12 @@ const InternalSpellsBar: React.FunctionComponent<SpellsBarProps> = React.memo(({
                     <div
                         className={classnames({
                             [styles.spellImage]: true,
-                            [styles.spellHightlight]: counter.toString() == clickedKey
+                            [styles.spellHightlight]: counter.toString() == clickedKey,
                         })}
-                        style={{ backgroundImage: `url(${activeSpell.image})` }}
+                        style={{
+                            backgroundImage: `url(${activeSpell.image})`,
+                            filter: checkIfDisabled(activeSpell.spellPowerCost),
+                        }}
                     />
                     <div className={styles.spellHolder}>
                         <SpellLoader cooldown={activeSpell.cooldown} castTime={spellCastTime[spellId]} />
--- libs/types/README.md
+++ libs/types/README.md
@@ -11,3 +11,11 @@ Run `nx test types` to execute the unit tests via [Jest](https://jestjs.io).
 Npm run start
 
 npm run start -- --project=engine
+
+## Run Creator
+
+npm run start -- --project=creator-web
+
+cd apps/creator/src
+
+go run .
