752e736ff68ff351ccc9f8c9d1ef40b219d3c7b1
YarynaHuk <jasmin.huk@gmail.com>
2024-02-07 14:07:13 +0100

    improved task number counting

--- apps/chives-and-dill/src/app/guiContent/quests/components/questSideView/QuestsSideView.tsx
+++ apps/chives-and-dill/src/app/guiContent/quests/components/questSideView/QuestsSideView.tsx
@@ -8,49 +8,58 @@ import { QuestStagePart } from '../questStagePart/QuestStagePart';
 import styles from './QuestsSideView.module.scss';
 
 export const QuestsSideView = () => {
-    const { setSelectedQuestId } = useContext(SelectedQuestProviderContext);
-    const { data: questProgress } = useEngineModuleReader(GlobalStoreModule.QUEST_PROGRESS);
-    const { data: questDefinition } = useEngineModuleReader(GlobalStoreModule.QUEST_DEFINITION);
+   const { setSelectedQuestId } = useContext(SelectedQuestProviderContext);
+   const { data: questProgress } = useEngineModuleReader(GlobalStoreModule.QUEST_PROGRESS);
+   const { data: questDefinition } = useEngineModuleReader(GlobalStoreModule.QUEST_DEFINITION);
 
-    const [showQuestsView, updateShowQuestsView] = useState(true);
+   const [showQuestsView, updateShowQuestsView] = useState(true);
 
-    let questsCounter = 0;
+   let questsCounter = 0;
 
-    const renderQuests = _.map(questProgress, (currentQuestProgress, questId) => {
-        const questStage = questDefinition[questId]?.stages[currentQuestProgress.activeStage];
-        questsCounter++;
+   const renderQuests = _.map(questProgress, (currentQuestProgress, questId) => {
+      const questStage = questDefinition[questId]?.stages[currentQuestProgress.activeStage];
+      if (!currentQuestProgress.allStagesCompleted) {
+         questsCounter++;
+      }
 
-        return (
-            <div className={styles.questMainContainer} key={questId}>
-                {currentQuestProgress.allStagesCompleted ? <div className={styles.questionMarkContainer}><img src={questionMark} className={styles.questionMark} /></div> : <div className={styles.questionMarkContainer}>{questsCounter}</div>}
-                <div className={styles.questDefinitionContainer}><div className={styles.questTitle} onClick={() => setSelectedQuestId(questId)}>
-                    {questDefinition[questId]?.name}
-                </div>
-                    <div className={styles.questDesc}>
-                        {_.map(questStage?.stageParts, (stagePart, stagePartId) => (
-                            <div className={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId].isDone ? styles.stagePartDone : ''}>
-                                <QuestStagePart
-                                    questStagePart={stagePart}
-                                    stagePartProgress={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId]}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div>
+      return (
+         <div className={styles.questMainContainer} key={questId}>
+            {currentQuestProgress.allStagesCompleted ? (
+               <div className={styles.questionMarkContainer}>
+                  <img src={questionMark} className={styles.questionMark} />
+               </div>
+            ) : (
+               <div className={styles.questionMarkContainer}>{questsCounter}</div>
+            )}
+            <div className={styles.questDefinitionContainer}>
+               <div className={styles.questTitle} onClick={() => setSelectedQuestId(questId)}>
+                  {questDefinition[questId]?.name}
+               </div>
+               <div className={styles.questDesc}>
+                  {_.map(questStage?.stageParts, (stagePart, stagePartId) => (
+                     <div className={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId].isDone ? styles.stagePartDone : ''}>
+                        <QuestStagePart
+                           questStagePart={stagePart}
+                           stagePartProgress={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId]}
+                        />
+                     </div>
+                  ))}
+               </div>
             </div>
-        );
-    });
+         </div>
+      );
+   });
 
-    return (
-        <div className={styles.questsSideViewContainer}>
-            <div className={styles.sectionTitleContainer}>
-                <div className={styles.sectionTitle}>Quests</div>
-                <button className={styles.questViewToggle} onClick={() => updateShowQuestsView(!showQuestsView)}>
-                    {showQuestsView ? '-' : '+'}
-                </button>
-            </div>
+   return (
+      <div className={styles.questsSideViewContainer}>
+         <div className={styles.sectionTitleContainer}>
+            <div className={styles.sectionTitle}>Quests</div>
+            <button className={styles.questViewToggle} onClick={() => updateShowQuestsView(!showQuestsView)}>
+               {showQuestsView ? '-' : '+'}
+            </button>
+         </div>
 
-            {showQuestsView ? renderQuests : null}
-        </div>
-    );
+         {showQuestsView ? renderQuests : null}
+      </div>
+   );
 };
