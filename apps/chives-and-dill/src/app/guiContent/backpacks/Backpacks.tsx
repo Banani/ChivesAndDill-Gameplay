61d0fa080f5b8207b9b3e6d5d835da8dcd66df33
WildPengu <7241679@wp.pl>
2024-03-29 12:07:45 +0100

    backpack WIP

--- apps/chives-and-dill/src/app/guiContent/backpacks/Backpacks.tsx
+++ apps/chives-and-dill/src/app/guiContent/backpacks/Backpacks.tsx
@@ -13,31 +13,6 @@ import styles from './Backpacks.module.scss';
 
 export const Backpacks = () => {
 
-  const dupa1 = {
-    1: { amount: 1, itemId: 'ItemInstance_0' },
-    2: { amount: 4, itemId: 'ItemInstance_1' },
-    3: { amount: 1, itemId: 'ItemInstance_5' },
-    4: { amount: 1, itemId: 'ItemInstance_10' },
-    5: { amount: 2, itemId: 'ItemInstance_14' },
-    6: { amount: 1, itemId: 'ItemInstance_16' },
-    7: { amount: 7, itemId: 'ItemInstance_13' },
-    8: { amount: 10, itemId: 'ItemInstance_16' },
-  }
-  const dupa2 = {
-    1: { amount: 1, itemId: 'ItemInstance_47' },
-    2: { amount: 2, itemId: 'ItemInstance_48' },
-    3: { amount: 1, itemId: 'ItemInstance_49' },
-    4: { amount: 1, itemId: 'ItemInstance_50' },
-    5: { amount: 3, itemId: 'ItemInstance_51' },
-    6: { amount: 4, itemId: 'ItemInstance_52' },
-    7: { amount: 7, itemId: 'ItemInstance_53' },
-  }
-
-  const hik = {
-    1: dupa1,
-    2: dupa2,
-  }
-
   const { data: backpackSchema } = useEngineModuleReader(GlobalStoreModule.BACKPACK_SCHEMA);
   const { data: backpackItems } = useEngineModuleReader(GlobalStoreModule.BACKPACK_ITEMS);
   const { activeCharacterId } = useEngineModuleReader(GlobalStoreModule.ACTIVE_CHARACTER).data;
@@ -45,19 +20,11 @@ export const Backpacks = () => {
   const [backpacksVisibility, changeBackpacksVisibility] = useState(true);
   const keyBoardContext = useContext(KeyBoardContext);
 
-  const { itemTemplates } = useItemTemplateProvider({ itemTemplateIds: _.map(dupa1, (item) => item.itemId) ?? [] });
-  // const backpackSlots = (
-  //   <div className={styles.itemSlotsContainer}>
-  //     {_.flatMap(hik, backpack => (
-  //       _.map(backpack, (slot, key) => (
-  //         <div key={slot.itemId} className={styles.slot}>
+  const arrayOfValues = _.flatMap(backpackItems[activeCharacterId], backpack => {
+    return _.map(backpack, item => item);
+  });
 
-  //           {/* <ItemIconPreview itemData={itemTemplates[]} highlight={ItemPreviewHighlight.icon} showMoney={true} /> */}
-  //         </div>
-  //       ))
-  //     ))}
-  //   </div>
-  // );
+  const { itemTemplates } = useItemTemplateProvider({ itemTemplateIds: arrayOfValues.map(item => item.itemId) });
 
   const backpackSlots = (
     <div className={styles.itemSlotsContainer}>
