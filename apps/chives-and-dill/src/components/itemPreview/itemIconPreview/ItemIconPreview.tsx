f2ca85eb146364c84f7349a45f29037f77a7a44a
WildPengu <7241679@wp.pl>
2024-03-30 15:20:16 +0100

    Correcting an error where the tooltip was beyond the screen

--- apps/chives-and-dill/src/components/itemPreview/itemIconPreview/ItemIconPreview.tsx
+++ apps/chives-and-dill/src/components/itemPreview/itemIconPreview/ItemIconPreview.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import styles from '../itemIconPreview/ItemIconPreview.module.scss';
 import { MoneyBar } from 'apps/chives-and-dill/src/app/guiContent/moneyBar/MoneyBar';
+import React, { useRef } from 'react';
+import styles from '../itemIconPreview/ItemIconPreview.module.scss';
+
+enum Dimension {
+    HEIGHT = "height",
+    WIDTH = "width"
+};
 
 export const ItemIconPreview = ({ itemData, highlight, showMoney }) => {
+
+    const tooltipContainer = useRef<HTMLDivElement>(null);
+    const tooltip = useRef<HTMLDivElement>(null);
 
     const checkIfStatIsAvailable = (stat, type) => {
         if(stat !== 0 && stat !== undefined && type !== 'Armor') {
@@ -14,9 +22,38 @@ export const ItemIconPreview = ({ itemData, highlight, showMoney }) => {
         };
     };
 
+    const checkIfObjectIsInScreen = (dimension: Dimension) => {
+        const { x, y } = tooltipContainer.current?.getBoundingClientRect() || {};
+        const { width, height } = tooltip.current?.getBoundingClientRect() || {};
+
+        const positionOfContainer = dimension === Dimension.HEIGHT ? y : x;
+        const sizeOfObject = dimension === Dimension.HEIGHT ? height : width;
+
+        const windowDimension = dimension === Dimension.HEIGHT ? window.innerHeight : window.outerWidth;
+
+        return positionOfContainer - sizeOfObject > 0 && positionOfContainer + sizeOfObject < windowDimension;
+    };
+
+    const getPositionForElement = (dimension: Dimension) => {
+        const { height: containerHeight, width: containerWidth } = tooltipContainer?.current?.getBoundingClientRect() || {};
+        const { height: tooltipHeight, width: tooltipWidth } = tooltip?.current?.getBoundingClientRect() || {};
+        const containerSize = dimension === Dimension.HEIGHT ? containerHeight : containerWidth;
+        const tooltipSize = dimension === Dimension.HEIGHT ? tooltipHeight : tooltipWidth;
+
+        if (checkIfObjectIsInScreen(dimension)) {
+            return containerSize;
+        }
+
+        return -tooltipSize;
+    };
+
     return (
-        <div style={{ backgroundImage: `url(${itemData.image})` }} className={styles.ItemImage + ` ${highlight ? styles.highlight : null}`} >
-            <div className={styles.ItemPreviewTooltip}>
+        <div style={{ backgroundImage: `url(${itemData.image})` }} ref={tooltipContainer} className={styles.ItemImage + ` ${highlight ? styles.highlight : null}`} >
+            <div
+                className={styles.ItemPreviewTooltip}
+                ref={tooltip}
+                style={{ bottom: getPositionForElement(Dimension.HEIGHT), left: getPositionForElement(Dimension.WIDTH) }}
+            >
                 <div className={styles.ItemPrevTooltipName}>{itemData.name}</div>
                 <div className={styles.ItemPrevTooltipLevel}>Item Level 1</div>
                 <div className={styles.ItemPrevTooltipSlot}>
