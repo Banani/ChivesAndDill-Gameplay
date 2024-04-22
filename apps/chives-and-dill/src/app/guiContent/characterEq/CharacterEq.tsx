7a3e1cbac50b2ce8838b4beb02551d0d2db8614e
YarynaHuk <129412519+WildPikachu9@users.noreply.github.com>
2024-04-22 12:35:00 +0200

    displayed advanced attrubutes in CharacterEq (#47)
    * displayed advanced attrubutes in CharacterEq
    
    * displayed advanced stats in eq tooltip
    
    * code refactoring
    
    * WIP

--- apps/chives-and-dill/src/app/guiContent/characterEq/CharacterEq.tsx
+++ apps/chives-and-dill/src/app/guiContent/characterEq/CharacterEq.tsx
@@ -12,129 +12,139 @@ import { SquareButton } from '../components/squareButton/SquareButton';
 import styles from './CharacterEq.module.scss';
 
 export const CharacterEq = () => {
-    const { activeCharacterId } = useEngineModuleReader(GlobalStoreModule.ACTIVE_CHARACTER).data;
-    const { data: equipment, lastUpdateTime: equipmentLastUpdateTime } = useEngineModuleReader(GlobalStoreModule.EQUIPMENT);
-    const { data: experience } = useEngineModuleReader(GlobalStoreModule.EXPERIENCE);
-    const { data: characters } = useEngineModuleReader(GlobalStoreModule.CHARACTER);
-    const { data: characterClasses } = useEngineModuleReader(GlobalStoreModule.CHARACTER_CLASS);
-    const { data: attributes } = useEngineModuleReader(GlobalStoreModule.ATTRIBUTES);
-    const activePlayer = characters[activeCharacterId];
-    const level = experience[activeCharacterId].level;
-    const characterClass = characterClasses[activePlayer.characterClassId];
-    const activePlayerAttributes = attributes[activeCharacterId];
+   const { activeCharacterId } = useEngineModuleReader(GlobalStoreModule.ACTIVE_CHARACTER).data;
+   const { data: equipment, lastUpdateTime: equipmentLastUpdateTime } = useEngineModuleReader(GlobalStoreModule.EQUIPMENT);
+   const { data: experience } = useEngineModuleReader(GlobalStoreModule.EXPERIENCE);
+   const { data: characters } = useEngineModuleReader(GlobalStoreModule.CHARACTER);
+   const { data: characterClasses } = useEngineModuleReader(GlobalStoreModule.CHARACTER_CLASS);
+   const { data: attributes } = useEngineModuleReader(GlobalStoreModule.ATTRIBUTES);
+   const activePlayer = characters[activeCharacterId];
+   const level = experience[activeCharacterId].level;
+   const characterClass = characterClasses[activePlayer.characterClassId];
+   const activePlayerAttributes = attributes[activeCharacterId];
 
-    const { callEngineAction } = useContext(EngineContext);
-    const { activeGlobalModal, setActiveGlobalModal } = useContext(ModalsManagerContext);
+   const { callEngineAction } = useContext(EngineContext);
+   const { activeGlobalModal, setActiveGlobalModal } = useContext(ModalsManagerContext);
 
-    const { itemTemplates, requestItemTemplate } = useContext(ItemTemplateContext);
+   const { itemTemplates, requestItemTemplate } = useContext(ItemTemplateContext);
 
-    useEffect(() => {
+   useEffect(() => {
+      if (activeGlobalModal !== GlobalModal.Equipment) {
+         return;
+      }
 
-        if (activeGlobalModal !== GlobalModal.Equipment) {
+      forEach(equipment[activeCharacterId], (itemReference) => {
+         if (!itemReference) {
             return;
-        }
+         }
 
-        forEach(equipment[activeCharacterId], (itemReference) => {
-            if (!itemReference) {
-                return;
-            }
+         if (!itemTemplates[itemReference.itemTemplateId]) {
+            requestItemTemplate(itemReference.itemTemplateId);
+         }
+      });
+   }, [itemTemplates, equipmentLastUpdateTime, requestItemTemplate, activeGlobalModal]);
 
-            if (!itemTemplates[itemReference.itemTemplateId]) {
-                requestItemTemplate(itemReference.itemTemplateId);
-            }
-        });
-    }, [itemTemplates, equipmentLastUpdateTime, requestItemTemplate, activeGlobalModal]);
+   const basic = ['armor', 'stamina', 'agility', 'intelect', 'strength', 'spirit'];
+   const advance = ['haste', 'criticalStrike', 'dodge', 'block'];
 
-    const stats = _.chain(activePlayerAttributes)
-        .pickBy((value, key) => value > 0)
-        .map((value, key) => (
-            <p key={key}>
-                <span className={styles.ChangeColor}>{`${key}: `}</span>
-                {value}
-            </p>
-        ))
-        .value();
+   const basicAttributes = _.pickBy(_.pick(activePlayerAttributes, basic), _.identity);
+   const advanceAttributes = _.pickBy(_.pick(activePlayerAttributes, advance), _.identity);
 
-    const renderItem = (itemSlot) => {
-        if (itemSlot && itemTemplates[itemSlot.itemTemplateId]) {
+   const generateStats = (attributes, isAdvanced = false) => {
+      return _.chain(attributes)
+         .pickBy((value, key) => value > 0)
+         .map((value, key) => {
+            const advancedAttrValue = (value / 10).toFixed(2);
+
             return (
-                <div
-                    className={styles.EqColumnsItem}
-                    onContextMenu={(e) => {
-                        e.preventDefault();
-                        callEngineAction({
-                            type: ItemClientActions.StripItem,
-                            itemInstanceId: itemSlot.itemInstanceId,
-                        });
-                    }}
-                >
-                    <ItemIconPreview
-                        itemTemplate={itemTemplates[itemSlot.itemTemplateId]}
-                        highlight={ItemPreviewHighlight.icon}
-                        showMoney={true}
-                    />
-                </div>
+               <p key={key}>
+                  <span className={styles.ChangeColor}>{`${key}: `}</span>
+                  {isAdvanced ? `${advancedAttrValue} %` : value}
+               </p>
             );
-        } else {
-            return <div className={styles.EqColumnsItem}></div>;
-        }
-    };
+         })
+         .value();
+   };
 
-    return activeGlobalModal === GlobalModal.Equipment ? (
-        <div className={styles.CharacterEqContainer}>
-            <img className={styles.CharacterEqIcon} src={characterClass.iconImage} />
-            <div className={styles.CharacterEqName}>{activePlayer.name}</div>
-            <div className={styles.ButtonContainer}>
-                <SquareButton onClick={() => setActiveGlobalModal(null)}>
-                    <CloseIcon />
-                </SquareButton>
+   const attributesStats = generateStats(basicAttributes);
+   const enhancementsStats = generateStats(advanceAttributes, true);
+
+   const renderItem = (itemSlot) => {
+      if (itemSlot && itemTemplates[itemSlot.itemTemplateId]) {
+         return (
+            <div
+               className={styles.EqColumnsItem}
+               onContextMenu={(e) => {
+                  e.preventDefault();
+                  callEngineAction({
+                     type: ItemClientActions.StripItem,
+                     itemInstanceId: itemSlot.itemInstanceId,
+                  });
+               }}
+            >
+               <ItemIconPreview itemTemplate={itemTemplates[itemSlot.itemTemplateId]} highlight={ItemPreviewHighlight.icon} showMoney={true} />
             </div>
-            <div className={styles.CharacterEqExpierence}>
-                Level: {level}
-                <span style={{ color: characterClass.color }}>{characterClass.name}</span>
+         );
+      } else {
+         return <div className={styles.EqColumnsItem}></div>;
+      }
+   };
+
+   return activeGlobalModal === GlobalModal.Equipment ? (
+      <div className={styles.CharacterEqContainer}>
+         <img className={styles.CharacterEqIcon} src={characterClass.iconImage} />
+         <div className={styles.CharacterEqName}>{activePlayer.name}</div>
+         <div className={styles.ButtonContainer}>
+            <SquareButton onClick={() => setActiveGlobalModal(null)}>
+               <CloseIcon />
+            </SquareButton>
+         </div>
+         <div className={styles.CharacterEqExpierence}>
+            Level: {level}
+            <span style={{ color: characterClass.color }}>{characterClass.name}</span>
+         </div>
+         <div className={styles.CharacterEqMain}>
+            <div className={styles.EquipmentContainer}>
+               <div className={styles.EqColumns}>
+                  {renderItem(equipment[activeCharacterId].head)}
+                  {renderItem(equipment[activeCharacterId].neck)}
+                  {renderItem(equipment[activeCharacterId].shoulder)}
+                  {renderItem(equipment[activeCharacterId].chest)}
+                  {renderItem(null)}
+                  {renderItem(null)}
+                  {renderItem(null)}
+                  {renderItem(equipment[activeCharacterId].wrist)}
+               </div>
+               <div className={styles.EqColumns}>
+                  {renderItem(equipment[activeCharacterId].hands)}
+                  {renderItem(equipment[activeCharacterId].waist)}
+                  {renderItem(equipment[activeCharacterId].legs)}
+                  {renderItem(equipment[activeCharacterId].feet)}
+                  {renderItem(equipment[activeCharacterId].finger1)}
+                  {renderItem(equipment[activeCharacterId].finger2)}
+                  {renderItem(equipment[activeCharacterId].trinket1)}
+                  {renderItem(equipment[activeCharacterId].trinket2)}
+               </div>
+               <div className={styles.EqColumns + ' ' + styles.BottomColumn}>
+                  {renderItem(null)}
+                  {renderItem(null)}
+               </div>
             </div>
-            <div className={styles.CharacterEqMain}>
-                <div className={styles.EquipmentContainer}>
-                    <div className={styles.EqColumns}>
-                        {renderItem(equipment[activeCharacterId].head)}
-                        {renderItem(equipment[activeCharacterId].neck)}
-                        {renderItem(equipment[activeCharacterId].shoulder)}
-                        {renderItem(equipment[activeCharacterId].chest)}
-                        {renderItem(null)}
-                        {renderItem(null)}
-                        {renderItem(null)}
-                        {renderItem(equipment[activeCharacterId].wrist)}
-                    </div>
-                    <div className={styles.EqColumns}>
-                        {renderItem(equipment[activeCharacterId].hands)}
-                        {renderItem(equipment[activeCharacterId].waist)}
-                        {renderItem(equipment[activeCharacterId].legs)}
-                        {renderItem(equipment[activeCharacterId].feet)}
-                        {renderItem(equipment[activeCharacterId].finger1)}
-                        {renderItem(equipment[activeCharacterId].finger2)}
-                        {renderItem(equipment[activeCharacterId].trinket1)}
-                        {renderItem(equipment[activeCharacterId].trinket2)}
-                    </div>
-                    <div className={styles.EqColumns + ' ' + styles.BottomColumn}>
-                        {renderItem(null)}
-                        {renderItem(null)}
-                    </div>
-                </div>
-                <div className={styles.AttributesContainer}>
-                    <div className={styles.EqLevelContainer}>
-                        <div className={styles.HeaderLevel + ' ' + styles.EqHeader}>Item Level</div>
-                        <div className={styles.NumberLevel}>0</div>
-                    </div>
-                    <div className={styles.EqAttributesContainer}>
-                        <div className={styles.HeaderAtt + ' ' + styles.EqHeader}>Attributes</div>
-                        <div className={styles.AttributesItems}>{stats}</div>
-                    </div>
-                    <div className={styles.EqEnhancementsContainer}>
-                        <div className={styles.HeaderEnh + ' ' + styles.EqHeader}>Enhancements</div>
-                        <div className={styles.EnhancementsItems}></div>
-                    </div>
-                </div>
+            <div className={styles.AttributesContainer}>
+               <div className={styles.EqLevelContainer}>
+                  <div className={styles.HeaderLevel + ' ' + styles.EqHeader}>Item Level</div>
+                  <div className={styles.NumberLevel}>0</div>
+               </div>
+               <div className={styles.EqAttributesContainer}>
+                  <div className={styles.HeaderAtt + ' ' + styles.EqHeader}>Attributes</div>
+                  <div className={styles.AttributesItems}>{attributesStats}</div>
+               </div>
+               <div className={styles.EqAttributesContainer}>
+                  <div className={styles.HeaderEnh + ' ' + styles.EqHeader}>Enhancements</div>
+                  <div className={styles.AttributesItems}>{enhancementsStats}</div>
+               </div>
             </div>
-        </div>
-    ) : null;
+         </div>
+      </div>
+   ) : null;
 };
--- apps/chives-and-dill/src/components/itemPreview/itemPreviewTooltip/components/EquipmentTooltip.tsx
+++ apps/chives-and-dill/src/components/itemPreview/itemPreviewTooltip/components/EquipmentTooltip.tsx
@@ -1,43 +1,49 @@
-
-
 import { EquipmentItemTemplate } from '@bananos/types';
 import { MoneyBar } from 'apps/chives-and-dill/src/app/guiContent/moneyBar/MoneyBar';
 import React from 'react';
-import styles from "./shared.module.scss";
+import styles from './shared.module.scss';
 
 interface EquipmentTooltipProps {
-    itemTemplate: EquipmentItemTemplate;
-    showMoney: boolean;
+   itemTemplate: EquipmentItemTemplate;
+   showMoney: boolean;
 }
 
 export const EquipmentTooltip: React.FunctionComponent<EquipmentTooltipProps> = ({ itemTemplate, showMoney }) => {
+   const renderPrimaryStat = (value, name, isAdvanced = false) => {
+      const style = isAdvanced ? { color: '#1eff00' } : {};
+      return value ? <div style={style}>{'+ ' + value + ' ' + name}</div> : null;
+   };
 
-    const renderPrimaryStat = (value, name) => {
-        return value ? <div>{'+ ' + value + ' ' + name}</div> : null;
-    };
+   const renderSecondaryStat = (value, name) => {
+      return value ? <div>{value + ' ' + name}</div> : null;
+   };
 
-    const renderSecondaryStat = (value, name) => {
-        return value ? <div>{value + ' ' + name}</div> : null;
-    };
-
-    return (<> <div className={styles.ItemPrevTooltipName}>{itemTemplate.name}</div>
-        <div className={styles.ItemPrevTooltipLevel}>Item Level 1</div>
-        <div className={styles.ItemPrevTooltipSlot}>
+   return (
+      <>
+         {' '}
+         <div className={styles.ItemPrevTooltipName}>{itemTemplate.name}</div>
+         <div className={styles.ItemPrevTooltipLevel}>Item Level 1</div>
+         <div className={styles.ItemPrevTooltipSlot}>
             <div>{itemTemplate.slot}</div>
             <div>Cloth</div>
-        </div>
-        <div className={styles.ItemPrevTooltipStat}>
+         </div>
+         <div className={styles.ItemPrevTooltipStat}>
             {renderSecondaryStat(itemTemplate.armor, 'Armor')}
             {renderPrimaryStat(itemTemplate.agility, 'Agility')}
             {renderPrimaryStat(itemTemplate.intelect, 'Intelect')}
             {renderPrimaryStat(itemTemplate.strength, 'Strength')}
             {renderPrimaryStat(itemTemplate.spirit, 'Spirit')}
             {renderPrimaryStat(itemTemplate.stamina, 'Stamina')}
-        </div>
-        {showMoney ? (
+            {renderPrimaryStat(itemTemplate.haste, 'Haste', true)}
+            {renderPrimaryStat(itemTemplate.criticalStrike, 'Crit Chance', true)}
+            {renderPrimaryStat(itemTemplate.dodge, 'Dodge', true)}
+            {renderPrimaryStat(itemTemplate.block, 'Block', true)}
+         </div>
+         {showMoney ? (
             <div className={styles.ItemPrevTooltipPrice}>
-                Sell price: <MoneyBar currency={itemTemplate.value} />
+               Sell price: <MoneyBar currency={itemTemplate.value} />
             </div>
-        ) : null}
-    </>)
-}
+         ) : null}
+      </>
+   );
+};
