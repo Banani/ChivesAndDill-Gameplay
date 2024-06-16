54f189acbe1af0563fcc51dafdd5abf8bdfc398f
YarynaHuk <jasmin.huk@gmail.com>
2024-06-16 13:07:06 +0200

    added missing uniq keys for components

--- apps/chives-and-dill/src/app/guiContent/quests/components/questDescription/QuestDescription.tsx
+++ apps/chives-and-dill/src/app/guiContent/quests/components/questDescription/QuestDescription.tsx
@@ -12,77 +12,75 @@ import _ from 'lodash';
 import styles from './QuestDescription.module.scss';
 
 interface QuestDescriptionProps {
-    questSchema: QuestSchema;
+   questSchema: QuestSchema;
 }
 
 export const QuestDescription: FunctionComponent<QuestDescriptionProps> = ({ questSchema }) => {
-    const { itemTemplates, requestItemTemplate } = useContext(ItemTemplateContext);
-    const { data: questProgress } = useEngineModuleReader(GlobalStoreModule.QUEST_PROGRESS);
+   const { itemTemplates, requestItemTemplate } = useContext(ItemTemplateContext);
+   const { data: questProgress } = useEngineModuleReader(GlobalStoreModule.QUEST_PROGRESS);
 
-    useEffect(() => {
-        forEach(questSchema.questReward.items, ({ itemTemplateId }) => {
-            if (!itemTemplates[itemTemplateId]) {
-                requestItemTemplate(itemTemplateId);
+   useEffect(() => {
+      forEach(questSchema.questReward.items, ({ itemTemplateId }) => {
+         if (!itemTemplates[itemTemplateId]) {
+            requestItemTemplate(itemTemplateId);
+         }
+      });
+   }, [itemTemplates, requestItemTemplate, questSchema.questReward.items]);
+
+   const items = useMemo(
+      () =>
+         map(questSchema.questReward.items, (item, index) => {
+            const itemTemplate = itemTemplates[item.itemTemplateId];
+            if (itemTemplate) {
+               return (
+                  <div className={styles.ItemContainer} key={index}>
+                     <ItemPreview itemTemplate={itemTemplate} showMoney={false} highlight={ItemPreviewHighlight.none} />
+                  </div>
+               );
             }
-        });
-    }, [itemTemplates, requestItemTemplate, questSchema.questReward.items]);
+         }),
+      [questSchema.questReward.items, itemTemplates]
+   );
 
-    const items = useMemo(
-        () =>
-            map(questSchema.questReward.items, (item) => {
-                const itemTemplate = itemTemplates[item.itemTemplateId];
-                if (itemTemplate) {
-                    return (
-                        <div className={styles.ItemContainer}>
-                            <ItemPreview
-                                itemTemplate={itemTemplate}
-                                showMoney={false}
-                                highlight={ItemPreviewHighlight.none}
-                            />
-                        </div>
-                    );
-                }
-            }),
-        [questSchema.questReward.items, itemTemplates]
-    );
+   const renderQuests = _.map(questProgress, (currentQuestProgress, questIndex) => {
+      const questStage = questSchema.stages[currentQuestProgress.activeStage];
 
-    const renderQuests = _.map(questProgress, (currentQuestProgress) => {
-        const questStage = questSchema.stages[currentQuestProgress.activeStage];
+      return (
+         <div className={styles.questDesc} key={questIndex}>
+            {_.map(questStage?.stageParts, (stagePart, stagePartId) => {
+               return (
+                  <div
+                     key={stagePartId}
+                     className={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId].isDone ? styles.stagePartDone : ''}
+                  >
+                     <QuestStagePart
+                        questStagePart={stagePart}
+                        stagePartProgress={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId]}
+                     />
+                  </div>
+               );
+            })}
+         </div>
+      );
+   });
 
-        return (
-            <div className={styles.questDesc}>
-                {_.map(questStage?.stageParts, (stagePart, stagePartId) => {
-
-                    return (
-                        <div className={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId].isDone ? styles.stagePartDone : ''}>
-                            <QuestStagePart
-                                questStagePart={stagePart}
-                                stagePartProgress={currentQuestProgress.stagesProgress[currentQuestProgress.activeStage][stagePartId]}
-                            />
-                        </div>
-                    )
-                })}
+   return (
+      <>
+         <h3 className={styles.SectionHeader}>{questSchema.name}</h3>
+         <div className={styles.SectionText}>{questSchema.description}</div>
+         <h3 className={styles.SectionHeader}>Quests Objectives</h3>
+         {renderQuests}
+         <h3 className={styles.SectionHeader}>Rewards</h3>
+         {items && <div className={styles.ItemsContainer}>{items}</div>}
+         {questSchema.questReward.currency && (
+            <div className={styles.MoneyReward}>
+               You will also receive:
+               <MoneyBar currency={questSchema.questReward.currency} />
             </div>
-        )
-    });
-
-    return (
-        <>
-            <h3 className={styles.SectionHeader}>{questSchema.name}</h3>
-            <div className={styles.SectionText}>{questSchema.description}</div>
-            <h3 className={styles.SectionHeader}>Quests Objectives</h3>
-            {renderQuests}
-            <h3 className={styles.SectionHeader}>Rewards</h3>
-            {items && <div className={styles.ItemsContainer}>{items}</div>}
-            {questSchema.questReward.currency && (
-                <div className={styles.MoneyReward}>
-                    You will also receive:
-                    <MoneyBar currency={questSchema.questReward.currency} />
-                </div>
-            )}
-            <div className={styles.ExperienceReward}>
-                Experience: <div className={styles.ExperienceAmount}>{questSchema.questReward.experience}</div>
-            </div>
-        </>
-    );
+         )}
+         <div className={styles.ExperienceReward}>
+            Experience: <div className={styles.ExperienceAmount}>{questSchema.questReward.experience}</div>
+         </div>
+      </>
+   );
 };
